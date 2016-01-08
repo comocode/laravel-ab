3b499efab21e214847b9ed5651907db26a42f467
Rulian Estivalletti <rulian.estivalletti@gmail.com>
2016-01-08 14:26:35 -0200

    movign to App

---
+++ src/App/Goal.php
@@ -0,0 +1,15 @@
+<?php namespace ComoCode\LaravelAb\App;
+
+class Goal extends \Eloquent {
+
+    protected $table = 'ab_goal';
+	protected $fillable = ['goal','value'];
+
+    public function experiment(){
+        return $this->belongsTo('ComoCode\LaravelAb\App\Experiment');
+    }
+
+    public function instance(){
+        return $this->belongsTo('ComoCode\LaravelAb\App\Instance');
+    }
+}
