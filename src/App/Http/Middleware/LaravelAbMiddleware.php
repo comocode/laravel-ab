f5d7474e1d6735ba6a22b5e0eebf66ae5395f813
Sajjad Rad <sajjad.273@gmail.com>
2016-03-10 14:00:28 +0330

    Renamed files

---
+++ src/App/Http/Middleware/LaravelAbMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ComoCode\LaravelAb\App\Http\Middleware;
+
+use ComoCode\LaravelAb\App\Ab;
+use Closure;
+
+class LaravelAbMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $response = $next($request);
+
+        $cookie = Ab::saveSession();
+        if (method_exists($response, 'withCookie')){
+            return $response->withCookie(cookie()->forever(config('laravel-ab.cache_key'), $cookie));
+        }
+        return $response;
+
+    }
+
+}
