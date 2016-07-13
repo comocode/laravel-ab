9ee0d127b7e1dce4c3c439429ba0f59bcdc51698
Rulian Estivalletti <rulian.estivalletti@gmail.com>
2016-07-13 15:21:11 -0300

    recommit changes due to weird git link issue

---
+++ src/App/Console/Commands/AbMigrate.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ComoCode\LaravelAb\App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+
+class AbMigrate extends Command
+{
+
+    protected $signature = 'ab:migrate
+    {--force : Bypasses prompt for user confirmation}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'migrates Laravel-Ab required tables';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->call('migrate', [
+            '--path' => str_replace(base_path(),'',realpath(__DIR__.'/../../../../migrations/')),
+            '--force' => $this->hasOption('force') ? true : false
+        ]);
+
+        $this->info("AB tables created successfully");
+
+    }
+}
