38942b4183cdad11ea6a0d5193186762445bdfb2
Rulian Estivalletti <rulian.estivalletti@gmail.com>
2016-07-18 13:34:02 -0300

    forget yaml, just do it in shell, duh

--- travis.build.sh
+++ travis.build.sh
@@ -1,43 +1,17 @@
 #!/usr/bin/env bash
-#
-# vim: set ft=sh:
-#
-# Based on https://gist.github.com/pkuczynski/8665367
 
-parse_yaml() {
-    local prefix=$2
-    local s
-    local w
-    local fs
-    s='[[:space:]]*'
-    w='[a-zA-Z0-9_]*'
-    fs="$(echo @|tr @ '\034')"
-    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
-    awk -F"$fs" '{
-    indent = length($1)/2;
-    vname[indent] = $2;
-    for (i in vname) {if (i > indent) {delete vname[i]}}
-        if (length($3) > 0) {
-            vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-            printf("%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, $3);
-        }
-    }' | sed 's/_=/+=/g'
-}
-
-eval $(parse_yaml .travis.yml)
-
-echo "________________"
-for i in "${before_install[@]}"
-do
-    `$i`
-done
-
-echo "testing...."
-`$script`
-
-echo "cleaning up...."
-for i in "${after_script[@]}"
-do
-    `$i`
-done
+composer self-update
+composer create-project laravel/laravel --prefer-dist build/laravel
+mkdir -p ./build/laravel/vendor/comocode
+ln -s `pwd` ./build/laravel/vendor/comocode/laravel-ab
+cd ./build/laravel
+touch storage/database.sqlite
+cp ./vendor/comocode/laravel-ab/tests/source/laravel/app.php ./config/app.php
+cp ./vendor/comocode/laravel-ab/tests/source/laravel/database.php ./config/database.php
+cp ./vendor/comocode/laravel-ab/tests/source/laravel/routes.php ./app/Http/routes.php
+sed -e 's/"app\/"/"app\/","ComoCode\\\\LaravelAb\\\\": "vendor\/comocode\/laravel-ab\/src"/' composer.json > temp.json && mv temp.json composer.json
+sed -e s/\)\;/\)/g ./vendor/composer/autoload_classmap.php > temp.php && mv temp.php ./vendor/composer/autoload_classmap.php
+cat ./vendor/comocode/laravel-ab/tests/source/laravel/autoload.php >> ./vendor/composer/autoload_classmap.php
+composer dump-autoload
+php artisan ab:migrate --force
+nohup php artisan serve &
