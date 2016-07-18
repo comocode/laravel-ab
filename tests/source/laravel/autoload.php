5f4b39203e1e52670dd635b690183bf451fd4016
Rulian Estivalletti <rulian.estivalletti@gmail.com>
2016-07-18 12:55:04 -0300

    updating composer install to current checkout for test passing

---
+++ tests/source/laravel/autoload.php
@@ -0,0 +1,12 @@
++ array(
+'ComoCode\\LaravelAb\\App\\Ab' => $vendorDir . '/comocode/laravel-ab/src/App/Ab.php',
+'ComoCode\\LaravelAb\\App\\Console\\Commands\\AbMigrate' => $vendorDir . '/comocode/laravel-ab/src/App/Console/Commands/AbMigrate.php',
+'ComoCode\\LaravelAb\\App\\Console\\Commands\\AbReport' => $vendorDir . '/comocode/laravel-ab/src/App/Console/Commands/AbReport.php',
+'ComoCode\\LaravelAb\\App\\Console\\Commands\\AbRollback' => $vendorDir . '/comocode/laravel-ab/src/App/Console/Commands/AbRollback.php',
+'ComoCode\\LaravelAb\\App\\Events' => $vendorDir . '/comocode/laravel-ab/src/App/Events.php',
+'ComoCode\\LaravelAb\\App\\Experiments' => $vendorDir . '/comocode/laravel-ab/src/App/Experiments.php',
+'ComoCode\\LaravelAb\\App\\Goal' => $vendorDir . '/comocode/laravel-ab/src/App/Goal.php',
+'ComoCode\\LaravelAb\\App\\Http\\Middleware\\LaravelAbMiddleware' => $vendorDir . '/comocode/laravel-ab/src/App/Http/Middleware/LaravelAbMiddleware.php',
+'ComoCode\\LaravelAb\\App\\Instance' => $vendorDir . '/comocode/laravel-ab/src/App/Instance.php',
+'ComoCode\\LaravelAb\\LaravelAbServiceProvider' => $vendorDir . '/comocode/laravel-ab/src/LaravelAbServiceProvider.php',
+);
